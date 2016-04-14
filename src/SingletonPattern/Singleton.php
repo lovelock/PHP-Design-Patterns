870e81906a3b67152225a5420484be352fecaf7e
lovelock <frostwong@gmail.com>
2016-04-14 17:48:52 +0800

    单例模式

---
+++ src/SingletonPattern/Singleton.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingchun1
+ * Date: 16/4/14
+ * Time: 下午5:45
+ */
+
+namespace DesignPatterns\SingletonPattern;
+
+
+class Singleton
+{
+    private static $_instance;
+
+    public static function getInstance()
+    {
+        if (! isset(self::$_instance)) {
+            self::$_instance = new static();
+        }
+
+        return self::$_instance;
+    }
+
+    private function __construct()
+    {
+        echo __METHOD__;
+    }
+
+    private function __wakeup()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+}
---
+++ src/SingletonPattern/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingchun1
+ * Date: 16/4/14
+ * Time: 下午5:47
+ */
+
+use DesignPatterns\SingletonPattern\Singleton;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$singleton = Singleton::getInstance();
