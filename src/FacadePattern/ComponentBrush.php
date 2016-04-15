209698a728bed5348eb7551d49e39b6f30514a8a
lovelock <frostwong@gmail.com>
2016-04-15 15:48:49 +0800

    门面模式

---
+++ src/FacadePattern/ComponentBrush.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingchun1
+ * Date: 16/4/15
+ * Time: 下午3:05
+ */
+
+namespace DesignPatterns\FacadePattern;
+
+
+class ComponentBrush
+{
+    public function brush()
+    {
+        echo "I am brushing\n";
+    }
+}
